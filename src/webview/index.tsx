bcbdd5832f537aca54574cdf2120f3a17c0decb4
emrantalukder <emran.talukder@gmail.com>
2023-11-03 19:50:58 -0500

    vscode web ui kit added

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { vscode } from "./utilities/vscode";
+import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
 
 const App = () => {
+  
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      const message = event.data;
+      switch (message.command) {
+        case 'showResource':
+          console.log(message);
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+  }, []);
 
   const handleClick = () => {
     console.log('clicked');
@@ -15,7 +34,7 @@ const App = () => {
   return <main>
     <h1>React App</h1>
     <p>React app in a VS Code extension</p>
-    <button onClick={handleClick}>Show Resource</button>
+    <VSCodeButton onClick={handleClick}>Show Resource</VSCodeButton>
   </main>;
 };
 
