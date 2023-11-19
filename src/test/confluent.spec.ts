2697c8214bde9f10077e339e842223f1bc5654d2
emrantalukder <emran.talukder@gmail.com>
2023-11-18 19:07:45 -0600

    mocha test config fixed

---
+++ src/test/confluent.spec.ts
@@ -0,0 +1,8 @@
+import * as assert from 'assert';
+import * as confluent from '../confluent';
+
+describe('Confluent API Test Suite', () => {
+    it('should pass', () => {
+        assert.equal(true, true);
+    });
+});
